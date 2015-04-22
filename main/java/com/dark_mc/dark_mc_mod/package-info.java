3d0bd472e218e819f6bc0d320adcbc6f55b98fec
grafvonzan <graf.von.zan@comcast.net>
2015-04-21 23:48:07 -0400

    Package Setup
    Resources and Com integrated

---
+++ main/java/com/dark_mc/dark_mc_mod/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author MVGS WT LOANER 8
+ *
+ */
+package com.dark_mc.dark_mc_mod;
---
+++ main/resources/assets/dark_mc/dark_mc_mod/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author MVGS WT LOANER 8
+ *
+ */
+package assets.dark_mc.dark_mc_mod;
