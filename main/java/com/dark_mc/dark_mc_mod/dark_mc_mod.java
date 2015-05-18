e587b8aa3a86c528a2499b523285bcb4995066e1
grafvonzan <graf.von.zan@comcast.net>
2015-05-18 12:03:36 -0400

    Package

---
+++ main/java/com/dark_mc/dark_mc_mod/dark_mc_mod.java
@@ -0,0 +1,16 @@
+package com.dark_mc.dark_mc_mod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid=dark_mc_mod.MODID, version=dark_mc_mod.VERSION)
+public class dark_mc_mod
+{
+	public static final String MODID = "dark_mc_mod";
+	public static final String VERSION = "0.0";
+	
+	
+	
+}
